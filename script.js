1ac11001fd89c450cfc7e7fcba7887bc92ecb7e3
Arun <arunmundakkal003@gmail.com>
2024-08-25 21:56:27 +0530

    third

--- script.js
+++ script.js
@@ -2,18 +2,21 @@ console.log("script")
 let search="movies";
 fetchData()
 async function fetchData(){
-
-    
     const url = `http://www.omdbapi.com/?s=${search}&apikey=66968ffe`;
     
      try{
         const responce =await fetch(url)
         const result= await responce.json()
         const list=result.Search
+        let list_movie=''
         list.map((item)=>{
             console.log(item)
+            list_movie+=`<div class="movie_container">
+                    <div class="titel"></div>
+                </div>`
         })
         console.log(result)
+        document.querySelector(".list").innerHTML=list_movie
     }
     catch(error){
         console.log(error)
