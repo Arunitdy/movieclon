4573e51a8ad7a0316b46bd9f6f165984ad480724
Arun <arunmundakkal003@gmail.com>
2024-09-01 00:33:39 +0530

    search

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ fetchData("marvel")
 
 //input taker
 document.querySelector(".search__input").addEventListener('keydown',function(event){
+  console.log(event.target.value)
     if(event.key==="Enter" && event.target.value!==""){
         event.preventDefault();
         const inputValue = event.target.value;
@@ -266,3 +267,32 @@ document.querySelector('.Login').addEventListener('click', () => {
     console.log("logout");
   }
 });
+
+//search result while typing
+
+document.querySelector(".search__input").addEventListener("input", async function(event){
+  console.log(event.target.value);
+  let arr=[];
+  const url = `http://www.omdbapi.com/?s=${search}&apikey=66968ffe`;
+  try{
+     const responce =await fetch(url)
+     const result= await responce.json()
+     const list=result.Search
+     let list_movie=''
+     list.map((item)=>{
+         console.log(item)
+         list_movie+=`<div class="search_container">
+                        <img src="${item.Poster}" class="search_movie_container" alt="${item.Titel}">
+                        <div class="Titel">${item.Titel}</div>
+                        <div class="year">${item.Year}</div>
+                      </div>`
+     })
+     console.log(result)
+     document.querySelector(".list").innerHTML=list_movie
+    }
+    catch(error){
+        console.log(error)
+        alert("check your internet connection")
+    }
+      
+})
